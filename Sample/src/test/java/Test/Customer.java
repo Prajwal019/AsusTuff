1a61687c6e422d9137cf67740018289051696062
Prajwal019 <129170313+Prajwal019@users.noreply.github.com>
2023-09-06 10:34:49 +0530

    Update Customer.java

--- Sample/src/test/java/Test/Customer.java
+++ Sample/src/test/java/Test/Customer.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 public class Customer {
 	@Test
 	public void addCustomer() {
+		//Customer
 		System.out.println("Customer added successfully");
 		System.out.println("Happy Customer");
 	}
